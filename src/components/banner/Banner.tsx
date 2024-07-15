945b73b4938c734eef8d554bd0a22b872a7d490d
hassandev <murad.dub@gmail.com>
2024-07-15 16:32:59 +0600

    some ui added

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,13 +1,3 @@
-// import bannerImg from "../../../public/assets/flowerBanner.jpg"
-// const Banner = () => {
-// return(
-//     <>
-//     <div className="rouded-md"><img className="w-full h-full rounded-md" src={bannerImg} alt="Flower Banner" /></div>
-//     </>
-// )
-// }
-// export default Banner
-
 import bannerImg from "../../../public/assets/flowerBanner.jpg"
 
 const Banner = () => {
@@ -24,21 +14,26 @@ const Banner = () => {
                 className="relative rounded-md w-full h-screen bg-cover bg-center flex items-center justify-center opacity-75"
                 style={{ backgroundImage: `url(${bannerImg})` }}
             >
-                <div className="text-center text-black">
+                <div className="text-center text-green-900">
+                    <h3 className="text-xl md:text-3xl mb-2">
+                        Welcome to
+                    </h3>
                     <h1 className="text-4xl md:text-6xl font-bold mb-4">
                         ONE OF THE LEADING ONLINE FLOWER SHOP
                     </h1>
+                    <h2 className="text-3xl md:text-5xl mb-4">
+                        Floral Fantasy
+                    </h2>
                     <button
                         onClick={scrollToNextSection}
-                        className="px-6 py-3 mt-4 bg-pink-500 hover:bg-pink-600 text-white rounded-md transition duration-300"
+                        className="px-6 py-3 mt-4 bg-lime-600 hover:bg-lime-500 text-white rounded-md transition duration-300"
                     >
                         Shop Now
                     </button>
                 </div>
             </div>
             {/* Dummy next section for demonstration */}
-            <div id="next-section" className="h-screen flex items-center justify-center bg-gray-200">
-                <h2 className="text-2xl">Next Section Content</h2>
+            <div id="next-section" className="flex items-center justify-center">
             </div>
         </>
     );
--- src/components/floralHome/ProductContainer.tsx
+++ src/components/floralHome/ProductContainer.tsx
@@ -5,19 +5,19 @@ import ProductCard from './ProductCard';
 import { useGetCategoriesQuery, useGetProductsQuery } from '@/redux/api/api';
 
 type TProduct = {
-    _id: string;
-    title: string;
-    price: number;
-    category: string;
-    quantity: number;
-    description: string;
-    rating: number;
-    image: string;
-  }
+  _id: string;
+  title: string;
+  price: number;
+  category: string;
+  quantity: number;
+  description: string;
+  rating: number;
+  image: string;
+}
 
 const ProductContainer = () => {
   const [category, setCategory] = useState('');
-  
+
   const { data: categories, isLoading: isCategoriesLoading, isError: isCategoriesError } = useGetCategoriesQuery(null);
   const { data: products, isLoading: isProductsLoading, isError: isProductsError } = useGetProductsQuery({ category });
 
@@ -28,7 +28,7 @@ const ProductContainer = () => {
         <CategoryFilter category={category} setCategory={setCategory} categories={categories?.data || []} />
       </div>
       <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px]">
-        <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
+        <div className="py-5 w-full h-full rounded-lg space-y-3">
           {isProductsLoading ? (
             <p>Loading...</p>
           ) : isProductsError ? (
