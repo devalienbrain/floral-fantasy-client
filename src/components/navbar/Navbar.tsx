43c289e0a30e23995ee7832a0b4191903f2398a4
sabbirhassanict <sabbirmurad.doict@gmail.com>
2024-07-27 16:03:28 +0600

    dynamically show cart total item on navbar done (successful)

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,15 @@
 import { FaCartPlus } from "react-icons/fa6";
 import logoIcon from "../../../public/assets/flowerLogo.png";
 import { Link } from "react-router-dom";
+import { useGetProductsQuery } from "@/redux/api/api";
 const Navbar = () => {
+  const {
+    data: productsData,
+  } = useGetProductsQuery({ addedToCart: true });
+
+    // Calculate total items and total price
+  const cartProducts = productsData?.data || [];
+  const totalItems = cartProducts.length;
   return (
     <>
       <div className="flex flex-col md:flex-row justify-center md:justify-between items-center p-7">
@@ -26,8 +34,8 @@ const Navbar = () => {
               <span>
                 <FaCartPlus className="text-3xl font-black" />
               </span>
-              <div className="w-6 h-6 border border-red-600 rounded-full absolute -top-3 -right-3 text-red-600 flex justify-center items-center font-semibold z-10">
-                5
+              <div className="w-6 h-6 border border-lime-900 rounded-full absolute -top-3 -right-3 text-lime-600 flex justify-center items-center font-bold z-10">
+                {totalItems}
               </div>
             </div>
           </Link>
